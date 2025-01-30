043f0c4638d5584746e0fa1c7a0cff3d3281cea8
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2025-01-30 17:51:12 +0100

    feat: Create Drawer for dasboard

--- src/layouts/Dashboard/Dashboard.jsx
+++ src/layouts/Dashboard/Dashboard.jsx
@@ -1,7 +1,51 @@
+import { FaHome } from "react-icons/fa";
+import { MdMenu } from "react-icons/md";
+import { NavLink, Outlet } from "react-router-dom";
+
 const Dashboard = () => {
+
+    const isAdmin = true;
+
     return (
-        <div>
-            <h1>Dashboard</h1>
+        <div className="flex min-h-screen">
+            <div className="drawer lg:drawer-open w-auto">
+                <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+                <div className="drawer-content p-3">
+                    {/* Page content here */}
+                    <label htmlFor="my-drawer-2" className="text-4xl drawer-button lg:hidden">
+                        <MdMenu />
+                    </label>
+                </div>
+                <div className="drawer-side z-50 lg:z-auto lg:relative fixed top-0 left-0 h-full">
+                    <label htmlFor="my-drawer-2" aria-label="close sidebar" className="drawer-overlay"></label>
+                    <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+                        {/* Sidebar content here */}
+                        {
+                            isAdmin ?
+                                <>
+                                    <li><NavLink to={"/dashboard/manageUser"}>Manage Users</NavLink></li>
+                                    <li><NavLink to={"/dashboard/manageCategory"}>Manage Category</NavLink></li>
+                                    <li><NavLink to={"/dashboard/paymentManagement"}>Payment Management</NavLink></li>
+                                    <li><NavLink to={"/dashboard/salesReport"}>Sales Report</NavLink></li>
+                                    <li><NavLink to={"/dashboard/manageBanner"}>Manage Banner Advertise</NavLink></li>
+                                </>
+                                :
+
+                                <></>
+                        }
+                        <div className="divider"></div>
+                        <li>
+                            <NavLink to={"/"}>
+                                <FaHome /> Home
+                            </NavLink>
+                        </li>
+
+                    </ul>
+                </div>
+            </div>
+            <div className="flex-1 p-4">
+                <Outlet />
+            </div>
         </div>
     );
 };
