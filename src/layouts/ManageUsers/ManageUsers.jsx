e12eaf4e59fb4a470d6149160942494a0fef3d47
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2025-01-31 00:19:55 +0100

    feat: Display all the users in tabular format

--- src/layouts/ManageUsers/ManageUsers.jsx
+++ src/layouts/ManageUsers/ManageUsers.jsx
@@ -1,7 +1,50 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
+import { FaTrash } from "react-icons/fa";
+
 const ManageUsers = () => {
+
+    const axiosSecure = useAxiosSecure();
+
+    const { data: users = [] } = useQuery({
+        queryKey: ['users'],
+        queryFn: async () => {
+            const res = await axiosSecure.get("/users");
+            return res.data
+        }
+    })
+
+
     return (
         <div>
-            <h1>Manage Users</h1>
+            <h1 className="text-center">Manage Users: {users.length}</h1>
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>User Name</th>
+                            <th>Email</th>
+                            <th>Role</th>
+                            <th>Delete User</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/* row 1 */}
+                        {
+                            users?.map((user, index) => <tr key={user._id}>
+                                <th>{index + 1}</th>
+                                <td>{user.name}</td>
+                                <td>{user.email}</td>
+                                <td>{user.role}</td>
+                                <td><button className="btn"><FaTrash /></button></td>
+                            </tr>)
+                        }
+
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
