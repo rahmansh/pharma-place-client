63d6380fdb9f8ea2910de539dea8ff9ddcbe49f6
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2025-01-30 22:51:00 +0100

    feat: Display dashboard options based on currently signed in user

--- src/layouts/Dashboard/Dashboard.jsx
+++ src/layouts/Dashboard/Dashboard.jsx
@@ -4,7 +4,7 @@ import { NavLink, Outlet } from "react-router-dom";
 
 const Dashboard = () => {
 
-    const isAdmin = true;
+    const role = 'admin';
 
     return (
         <div className="flex min-h-screen">
@@ -21,17 +21,46 @@ const Dashboard = () => {
                     <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4">
                         {/* Sidebar content here */}
                         {
-                            isAdmin ?
-                                <>
-                                    <li><NavLink to={"/dashboard/manageUser"}>Manage Users</NavLink></li>
-                                    <li><NavLink to={"/dashboard/manageCategory"}>Manage Category</NavLink></li>
-                                    <li><NavLink to={"/dashboard/paymentManagement"}>Payment Management</NavLink></li>
-                                    <li><NavLink to={"/dashboard/salesReport"}>Sales Report</NavLink></li>
-                                    <li><NavLink to={"/dashboard/manageBanner"}>Manage Banner Advertise</NavLink></li>
-                                </>
-                                :
-
-                                <></>
+                            role === 'admin' &&
+                            <>
+                                <li>
+                                    <NavLink to={"/dashboard/manageUser"}>Manage Users</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to={"/dashboard/manageCategory"}>Manage Category</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to={"/dashboard/paymentManagement"}>Payment Management</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to={"/dashboard/salesReport"}>Sales Report</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to={"/dashboard/manageBanner"}>Manage Banner Advertise</NavLink>
+                                </li>
+                            </>
+                        }
+                        {
+                            role === 'seller' &&
+                            <>
+                                <li>
+                                    <NavLink to={"/dashboard/manageMedicines"}>Manage Medicines</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to={"/dashboard/paymentHistory"}>Payment History</NavLink>
+                                </li>
+                                <li>
+                                    <NavLink to={"/dashboard/askAdvertisement"}>Ask For Advertisement</NavLink>
+                                </li>
+                            </>
+                        }
+                        {
+                            role === 'user' &&
+                            <>
+                                <li>
+                                    <NavLink to={"/dashboard/manageMedicines"}>Manage Medicines</NavLink>
+                                </li>
+                            </>
                         }
                         <div className="divider"></div>
                         <li>
