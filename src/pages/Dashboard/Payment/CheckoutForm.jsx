9d1373c9cfa9f0d2ac7e78837decfec2de1426e1
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2025-02-08 01:35:36 +0100

    feat: Display error message

--- src/pages/Dashboard/Payment/CheckoutForm.jsx
+++ src/pages/Dashboard/Payment/CheckoutForm.jsx
@@ -1,6 +1,8 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { useState } from "react";
 
 const CheckoutForm = () => {
+    const [error, setError] = useState('')
     const stripe = useStripe();
     const elements = useElements();
 
@@ -25,8 +27,10 @@ const CheckoutForm = () => {
 
         if (error) {
             console.log('[error]', error);
+            setError(error.message)
         } else {
             console.log('[PaymentMethod]', paymentMethod)
+            setError('');
         }
 
     }
@@ -52,6 +56,7 @@ const CheckoutForm = () => {
             <button className="btn btn-sm btn-primary my-4" type="submit" disabled={!stripe}>
                 Pay
             </button>
+            <p className="text-red-600">{error}</p>
         </form>
     );
 };
