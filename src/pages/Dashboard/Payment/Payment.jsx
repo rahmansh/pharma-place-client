1bfa6609edbe921593b721e484621ed293d29931
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2025-02-08 01:33:42 +0100

    feat: Create payment form and take payment

--- src/pages/Dashboard/Payment/Payment.jsx
+++ src/pages/Dashboard/Payment/Payment.jsx
@@ -1,7 +1,20 @@
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
+import CheckoutForm from "./CheckoutForm";
+
+
+
+const stripePromise = loadStripe(import.meta.env.VITE_PAYMENT_GATEWAY_PK);
+
 const Payment = () => {
     return (
         <div>
-            <h1>Payment</h1>
+            <div>
+                <h1 className='text-center text-3xl mt-3 mb-8'>Payment Page</h1>
+            </div>
+            <Elements stripe={stripePromise}>
+                <CheckoutForm />
+            </Elements>
         </div>
     );
 };
