22606563d776c3a89d5dd6e280b81d53fb2a3d29
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2025-02-09 00:51:00 +0100

    feat: Display medcines added by currently logged in user

--- src/pages/Dashboard/Seller/ManageMedicines/ManageMedicines.jsx
+++ src/pages/Dashboard/Seller/ManageMedicines/ManageMedicines.jsx
@@ -1,7 +1,55 @@
+import { useEffect, useState } from "react";
+import useAuth from "../../../../hooks/useAuth";
+import useAxiosSecure from "../../../../hooks/useAxiosSecure";
+
 const ManageMedicines = () => {
+    const [medicines, setMedicines] = useState([]);
+    const { user } = useAuth();
+    const axiosSecure = useAxiosSecure();
+    console.log(user)
+
+    console.log(medicines)
+
+    useEffect(() => {
+        axiosSecure.get(`/medicines/${user.email}`)
+            .then(res => setMedicines(res.data))
+
+    }, [axiosSecure, user?.email])
+
+
     return (
         <div>
-            <h1>Manage Medicines</h1>
+            <h1 className="text-center my-3">Manage Medicines</h1>
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Item name</th>
+                            <th>Generic Name</th>
+                            <th>Category</th>
+                            <th>Company</th>
+                            <th>Price</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/* row 1 */}
+                        {
+                            medicines.map((medicine, index) =>
+                                <tr key={medicine._id}>
+                                    <th>{index + 1}</th>
+                                    <td>{medicine.name}</td>
+                                    <td>{medicine.genericName}</td>
+                                    <td>{medicine.category}</td>
+                                    <td>{medicine.company}</td>
+                                    <td>{medicine.price}</td>
+                                </tr>)
+                        }
+
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
