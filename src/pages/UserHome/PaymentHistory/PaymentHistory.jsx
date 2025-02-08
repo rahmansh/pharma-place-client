bb620b15b37703194a1d6bb1b13a2b641b211bf6
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2025-02-08 14:33:40 +0100

    feat: Display payment information of current user

--- src/pages/UserHome/PaymentHistory/PaymentHistory.jsx
+++ src/pages/UserHome/PaymentHistory/PaymentHistory.jsx
@@ -1,7 +1,49 @@
+import { useQuery } from "@tanstack/react-query";
+import useAuth from "../../../hooks/useAuth";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+
 const PaymentHistory = () => {
+    const { user } = useAuth();
+    const axiosSecure = useAxiosSecure()
+
+    const { data: payments = [] } = useQuery({
+        queryKey: ['payments', user.email],
+        queryFn: async () => {
+            const res = await axiosSecure.get(`/payments/${user.email}`)
+            return res.data;
+        }
+    })
+
+    console.log('Payments', payments)
+
     return (
         <div>
-            <h1>User Payment History</h1>
+            <h2 className="text-3xl text-center mt-5 mb-5">Total Payments: {payments?.length}</h2>
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Price</th>
+                            <th>Transaction Id</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/* row 1 */}
+                        {
+                            payments.map((payment, index) => <tr key={payment._id}>
+                                <th>{index + 1}</th>
+                                <td>{payment.price}</td>
+                                <td>{payment.transactionId}</td>
+                                <td>{payment.status}</td>
+                            </tr>
+                            )
+                        }
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
