5d92d296bae656566c8d67a4a267fc11e6ff154c
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2025-02-09 02:41:12 +0100

    feat: Create form to get medicine info

--- src/pages/Dashboard/Seller/ManageMedicines/ManageMedicines.jsx
+++ src/pages/Dashboard/Seller/ManageMedicines/ManageMedicines.jsx
@@ -1,14 +1,20 @@
 import { useEffect, useState } from "react";
 import useAuth from "../../../../hooks/useAuth";
 import useAxiosSecure from "../../../../hooks/useAxiosSecure";
+import { useForm } from "react-hook-form"
+
 
 const ManageMedicines = () => {
+    const { register, handleSubmit } = useForm();
     const [medicines, setMedicines] = useState([]);
     const { user } = useAuth();
     const axiosSecure = useAxiosSecure();
-    console.log(user)
 
-    console.log(medicines)
+    const [isOpen, setIsOpen] = useState(false)
+
+    const onSubmit = async (data) => {
+        console.log(data)
+    };
 
     useEffect(() => {
         axiosSecure.get(`/medicines/${user.email}`)
@@ -50,6 +56,92 @@ const ManageMedicines = () => {
                     </tbody>
                 </table>
             </div>
+
+            {isOpen && (
+                <div className="modal modal-open" role="dialog">
+                    <div className="modal-box text-center">
+                        <div className="max-w-4xl mx-auto">
+                            <form onSubmit={handleSubmit(onSubmit)}>
+                                {/* Medicine Name */}
+                                <div>
+                                    <label className="form-control w-full">
+                                        <div className="label">
+                                            <span className="text-black">Medicine Name*</span>
+                                        </div>
+                                        <input {...register("name")} type="text" placeholder="Medicine Name" className="input input-bordered w-full bg-white" />
+                                    </label>
+                                </div>
+                                {/* Generic Name */}
+                                <div>
+                                    <label className="form-control w-full">
+                                        <div className="label">
+                                            <span className="text-black">Generic Name*</span>
+                                        </div>
+                                        <input {...register("genericName")} type="text" placeholder="Generic Name" className="input input-bordered w-full bg-white" />
+                                    </label>
+                                </div>
+                                {/* Description */}
+                                <div>
+                                    <label className="form-control w-full">
+                                        <div className="label">
+                                            <span className="text-black">Description</span>
+                                        </div>
+                                        <input {...register("description")} type="text" placeholder="Description" className="input input-bordered w-full bg-white" />
+                                    </label>
+                                </div>
+                                <div className="flex gap-6 my-4">
+                                    {/* Price */}
+                                    <div>
+                                        <label className="form-control w-full">
+                                            <div className="label">
+                                                <span className="text-black">Price</span>
+                                            </div>
+                                            <input {...register("description")} type="number" placeholder="Price" className="input input-bordered w-full bg-white" />
+                                        </label>
+                                    </div>
+                                    {/* Category Dropdown */}
+                                    <div className="w-full">
+                                        <div className="label">
+                                            <span className="text-black">Category*</span>
+                                        </div>
+                                        <select defaultValue={"default"} {...register("category")} className="select bg-white w-full">
+                                            <option value={"default"}>Select Category</option>
+                                            <option value="salad">Salad</option>
+                                            <option value="pizza">Pizza</option>
+                                            <option value="soups">Soups</option>
+                                            <option value="desserts">Desserts</option>
+                                            <option value="drinks">Drinks</option>
+                                        </select>
+                                    </div>
+                                </div>
+
+                                {/* Company Dropdown */}
+                                <div className="w-full">
+                                    <div className="label">
+                                        <span className="text-black">Company*</span>
+                                    </div>
+                                    <select defaultValue={"default"} {...register("category")} className="select bg-white w-full">
+                                        <option value={"default"}>Select Category</option>
+                                        <option value="salad">Salad</option>
+                                        <option value="pizza">Pizza</option>
+                                        <option value="soups">Soups</option>
+                                        <option value="desserts">Desserts</option>
+                                        <option value="drinks">Drinks</option>
+                                    </select>
+                                </div>
+                                {/* image upload */}
+                                <div className="my-6">
+                                    <input {...register('image')} type="file" className="file-input w-full max-w-xs bg-white" />
+                                </div>
+                                <input className="btn btn-primary mt-4" value="Add Item" type="submit" />
+                            </form>
+                        </div>
+                    </div>
+                    <div className="modal-backdrop" onClick={() => setIsOpen(false)}></div>
+                </div>
+            )}
+
+            <button onClick={() => setIsOpen(true)} className="btn btn-primary mt-6">Add Medicine</button>
         </div>
     );
 };
