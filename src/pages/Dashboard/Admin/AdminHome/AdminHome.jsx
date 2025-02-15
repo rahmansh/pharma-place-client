8ccf019196279897bcc682afdc5f21de545e9cf9
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2025-02-15 01:47:58 +0100

    feat: Display stats in Admin Home page

--- src/pages/Dashboard/Admin/AdminHome/AdminHome.jsx
+++ src/pages/Dashboard/Admin/AdminHome/AdminHome.jsx
@@ -1,7 +1,42 @@
+import { useQuery } from "@tanstack/react-query";
+import useAuth from "../../../../hooks/useAuth";
+import useAxiosSecure from "../../../../hooks/useAxiosSecure";
+
 const AdminHome = () => {
+    const { user } = useAuth();
+
+    const axiosSecure = useAxiosSecure();
+
+    const { data: stats } = useQuery({
+        queryKey: ['admin-stats'],
+        queryFn: async () => {
+            const res = await axiosSecure.get('/admin-stats')
+            return res.data;
+        }
+    })
+
+
     return (
         <div>
-            <h1>Admin Home</h1>
+            <h1 className="text-3xl text-center">Revenue</h1>
+            <div className="text-center mt-6">
+                <div className="stats stats-vertical lg:stats-horizontal shadow">
+                    <div className="stat">
+                        <div className="stat-title">Users</div>
+                        <div className="stat-value">{stats.users}</div>
+                    </div>
+                    <div className="stat">
+                        <div className="stat-title">Revenue</div>
+                        <div className="stat-value">{stats.revenue}</div>
+
+                    </div>
+                    <div className="stat">
+                        <div className="stat-title">Orders</div>
+                        <div className="stat-value">{stats.orders}</div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     );
 };
