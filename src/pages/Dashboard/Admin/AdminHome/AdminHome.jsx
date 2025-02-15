b76a18c120d73bd6e45b60265bee1c1d39636d16
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2025-02-15 02:15:05 +0100

    Fix: render conditionally

--- src/pages/Dashboard/Admin/AdminHome/AdminHome.jsx
+++ src/pages/Dashboard/Admin/AdminHome/AdminHome.jsx
@@ -1,10 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
-import useAuth from "../../../../hooks/useAuth";
 import useAxiosSecure from "../../../../hooks/useAxiosSecure";
 
 const AdminHome = () => {
-    const { user } = useAuth();
-
     const axiosSecure = useAxiosSecure();
 
     const { data: stats } = useQuery({
@@ -23,16 +20,16 @@ const AdminHome = () => {
                 <div className="stats stats-vertical lg:stats-horizontal shadow">
                     <div className="stat">
                         <div className="stat-title">Users</div>
-                        <div className="stat-value">{stats.users}</div>
+                        <div className="stat-value">{stats?.users}</div>
                     </div>
                     <div className="stat">
                         <div className="stat-title">Revenue</div>
-                        <div className="stat-value">{stats.revenue}</div>
+                        <div className="stat-value">{stats?.revenue}</div>
 
                     </div>
                     <div className="stat">
                         <div className="stat-title">Orders</div>
-                        <div className="stat-value">{stats.orders}</div>
+                        <div className="stat-value">{stats?.orders}</div>
                     </div>
                 </div>
             </div>
