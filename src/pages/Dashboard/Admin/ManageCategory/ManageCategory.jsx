660f5793bfdc634307c8456a3c90f41c4c677bda
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2025-02-15 02:45:22 +0100

    feat: Implement category management functionality
    - Added loading state handling for category data retrieval.
    - Integrated Axios for fetching categories from the API.
    - Created a table to display categories with edit and delete options.

--- src/pages/Dashboard/Admin/ManageCategory/ManageCategory.jsx
+++ src/pages/Dashboard/Admin/ManageCategory/ManageCategory.jsx
@@ -1,7 +1,52 @@
+import { FaEdit, FaTrash } from "react-icons/fa";
+import useAxiosPublic from "../../../../hooks/useAxiosPublic";
+import { useQuery } from "@tanstack/react-query";
 const ManageCategory = () => {
+
+    const axiosPublic = useAxiosPublic();
+    const { data: categories, isLoading } = useQuery({
+        queryKey: ["categories"],
+        queryFn: async () => {
+            const res = await axiosPublic.get("/categories");
+            return res.data;
+        }
+    })
+
+    if (isLoading) {
+        return <div className="flex justify-center items-center h-screen">Loading...</div>
+    }
+
     return (
         <div>
             <h1>Manage Category</h1>
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Category Name</th>
+                            <th>Update</th>
+                            <th>Delete</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/* row 1 */}
+
+                        {
+                            categories.map((category, index) => (
+                                <tr key={category._id}>
+                                    <th>{index + 1}</th>
+                                    <td>{category.categoryName}</td>
+                                    <td><button><FaEdit /></button></td>
+                                    <td><button><FaTrash /></button></td>
+                                </tr>
+                            ))
+                        }
+
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
