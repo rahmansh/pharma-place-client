b6f7ecead7dd7622e9cb3aac358841ace36f628c
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2025-02-16 23:15:17 +0100

    feat: Add payment history page

--- src/pages/Dashboard/Seller/PaymentHistory/PaymentHistory.jsx
+++ src/pages/Dashboard/Seller/PaymentHistory/PaymentHistory.jsx
@@ -1,7 +1,7 @@
 const PaymentHistory = () => {
     return (
         <div>
-            <h1>Seller Payment History</h1>
+            <h1 className="text-3xl text-center">Seller Payment History</h1>
         </div>
     );
 };
