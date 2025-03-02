7879eccae79054eb5d3c7aa4fa426f85a474ad1d
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2025-03-03 00:55:28 +0100

    feat: Display Discounted product

--- src/pages/Home/DiscountProducts/DiscountProducts.jsx
+++ src/pages/Home/DiscountProducts/DiscountProducts.jsx
@@ -6,54 +6,66 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 
 import 'swiper/css';
 import 'swiper/css/pagination';
-import 'swiper/css/navigation';
+import 'swiper/css/free-mode';
 
 
 
-import { Autoplay, Pagination, Navigation } from 'swiper/modules';
+
+import { FreeMode, Pagination } from 'swiper/modules';
+import useAxiosPublic from '../../../hooks/useAxiosPublic';
+import { useQuery } from '@tanstack/react-query';
 
 const DiscountProducts = () => {
     const progressCircle = useRef(null);
     const progressContent = useRef(null);
+
+    const axiosPublic = useAxiosPublic();
+
+    const { data: products } = useQuery({
+        queryKey: ["discounted-product"],
+        queryFn: async () => {
+            const res = await axiosPublic.get("/medicines/discount");
+            return res.data.data;
+        }
+    })
+
+
+    console.log(products)
+
+
+
     const onAutoplayTimeLeft = (s, time, progress) => {
         progressCircle.current.style.setProperty('--progress', 1 - progress);
         progressContent.current.textContent = `${Math.ceil(time / 1000)}s`;
     };
 
     return (
-        <div className="text-center text-3xl mt-12">
-            <h1>Discounted Products</h1>
+        <div className="text-center text-3xl mt-12 mb-20">
+            <h1 className='mt-20 mb-10 font-bold'>Discounted Products</h1>
             <div>
                 <Swiper
+                    slidesPerView={3}
                     spaceBetween={30}
-                    centeredSlides={true}
-                    autoplay={{
-                        delay: 2500,
-                        disableOnInteraction: false,
-                    }}
+                    freeMode={true}
                     pagination={{
                         clickable: true,
                     }}
-                    navigation={true}
-                    modules={[Autoplay, Pagination, Navigation]}
-                    onAutoplayTimeLeft={onAutoplayTimeLeft}
+                    modules={[FreeMode, Pagination]}
                     className="mySwiper"
+                    style={{ height: '600px' }}
                 >
-                    <SwiperSlide>Slide 1</SwiperSlide>
-                    <SwiperSlide>Slide 2</SwiperSlide>
-                    <SwiperSlide>Slide 3</SwiperSlide>
-                    <SwiperSlide>Slide 4</SwiperSlide>
-                    <SwiperSlide>Slide 5</SwiperSlide>
-                    <SwiperSlide>Slide 6</SwiperSlide>
-                    <SwiperSlide>Slide 7</SwiperSlide>
-                    <SwiperSlide>Slide 8</SwiperSlide>
-                    <SwiperSlide>Slide 9</SwiperSlide>
-                    <div className="autoplay-progress" slot="container-end">
-                        <svg viewBox="0 0 48 48" ref={progressCircle}>
-                            <circle cx="24" cy="24" r="20"></circle>
-                        </svg>
-                        <span ref={progressContent}></span>
-                    </div>
+                    {
+                        products.map(product => <div key={product._id}>
+                            <SwiperSlide>
+                                <img src={product.image} alt="" className="w-full h-full object-cover rounded-lg" />
+
+                                <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-50 flex flex-col justify-center items-center text-white p-4">
+                                    <h3 className="text-3xl font-bold">{product.name}</h3>
+                                    <p className="text-lg">Discount: {product.discount}%</p>
+                                </div>
+                            </SwiperSlide>
+                        </div>)
+                    }
                 </Swiper>
             </div>
         </div>
