539addbc088e1729a12f8157a52f2d196c01e569
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2025-03-03 00:32:02 +0100

    feat: Add swiper

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Banner from "../Banner/Banner";
 import Categories from "../Categories/Categories";
+import DiscountProducts from "../DiscountProducts/DiscountProducts";
 
 const Home = () => {
     return (
         <div>
             <Banner />
             <Categories />
+            <DiscountProducts />
         </div>
     );
 };
