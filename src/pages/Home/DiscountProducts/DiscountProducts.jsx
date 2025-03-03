a722afb2948867ae8fccd13679cc0ad6c0f404e8
Sajedur Rahman <70533256+SajedurRahmanShihab@users.noreply.github.com>
2025-03-03 01:42:55 +0100

    feat: Display message when no discount available

--- src/pages/Home/DiscountProducts/DiscountProducts.jsx
+++ src/pages/Home/DiscountProducts/DiscountProducts.jsx
@@ -17,7 +17,7 @@ const DiscountProducts = () => {
 
     const axiosPublic = useAxiosPublic();
 
-    const { data: products } = useQuery({
+    const { data: products, isLoading } = useQuery({
         queryKey: ["discounted-product"],
         queryFn: async () => {
             const res = await axiosPublic.get("/medicines/discount");
@@ -29,30 +29,38 @@ const DiscountProducts = () => {
         <div className="text-center text-3xl mt-12 mb-20">
             <h1 className='mt-20 mb-10 font-bold'>Discounted Products</h1>
             <div>
-                <Swiper
-                    slidesPerView={3}
-                    spaceBetween={30}
-                    freeMode={true}
-                    pagination={{
-                        clickable: true,
-                    }}
-                    modules={[FreeMode, Pagination]}
-                    className="mySwiper"
-                    style={{ height: '600px' }}
-                >
-                    {
-                        products?.map(product => <div key={product._id}>
-                            <SwiperSlide>
-                                <img src={product.image} alt="" className="w-full h-full object-cover rounded-lg" />
+                {
+                    products?.length > 0 ? (
+                        <Swiper
+                            slidesPerView={3}
+                            spaceBetween={30}
+                            freeMode={true}
+                            pagination={{
+                                clickable: true,
+                            }}
+                            modules={[FreeMode, Pagination]}
+                            className="mySwiper"
+                            style={{ height: '600px' }}
+                        >
+                            {
+                                products?.map(product => <div key={product._id}>
+                                    <SwiperSlide>
+                                        <img src={product.image} alt="" className="w-full h-full object-cover rounded-lg" />
 
-                                <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-50 flex flex-col justify-center items-center text-white p-4">
-                                    <h3 className="text-3xl font-bold">{product.name}</h3>
-                                    <p className="text-lg">Discount: {product.discount}%</p>
-                                </div>
-                            </SwiperSlide>
-                        </div>)
-                    }
-                </Swiper>
+                                        <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-50 flex flex-col justify-center items-center text-white p-4">
+                                            <h3 className="text-3xl font-bold">{product.name}</h3>
+                                            <p className="text-lg">Discount: {product.discount}%</p>
+                                        </div>
+                                    </SwiperSlide>
+                                </div>)
+                            }
+                        </Swiper>
+                    ) : <>
+                        {
+                            !isLoading && <p className="text-lg text-gray-500">No Discount Available Currently</p>
+                        }
+                    </>
+                }
             </div>
         </div>
     );
